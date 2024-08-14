63aeba71ac17c40f53179b68cdbbcb63280f4318
johannaj16 <johannaj@vt.edu>
2024-08-13 23:58:26 -0500

    finally working in android studio :D

--- easybite/app/(tabs)/profile.tsx
+++ easybite/app/(tabs)/profile.tsx
@@ -1,36 +1,36 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, Text, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from '../../components/HelloWave';
+import ParallaxScrollView from '../../components/ParallaxScrollView';
+import { ThemedText } from '../../components/ThemedText';
+import { ThemedView } from '../../components/ThemedView';
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">profile page!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+    headerImage={
+      <Image
+        source={require('../../assets/images/partial-react-logo.png')}
+        style={styles.reactLogo}
+      />
+    }>
+    <ThemedView style={styles.titleContainer}>
+      <ThemedText type="title">profile page!</ThemedText>
+      <HelloWave />
+    </ThemedView>
+    <ThemedView style={styles.stepContainer}>
+      <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+      <ThemedText>
+        Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+        Press{' '}
+        <ThemedText type="defaultSemiBold">
+          {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
+        </ThemedText>{' '}
+        to open developer tools.
+      </ThemedText>
+    </ThemedView>
+  </ParallaxScrollView>
   );
 }
 
